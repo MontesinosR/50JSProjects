b570d099398fac7026f816095a2241082b5622dd
Carlos <carlosmontesinos1990@gmail.com>
2022-03-01 11:38:38 +0000

    dark-light mode working properly

--- Project 19 - Theme Clock/script.js
+++ Project 19 - Theme Clock/script.js
@@ -7,3 +7,14 @@ const toggleEl = document.querySelector('.toggle')
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggleEl.addEventListener('click', (e) => {
+  const html = document.querySelector('html')
+  if(html.classList.contains('dark')) {
+    html.classList.remove('dark')
+    e.target.innerHTML = 'Dark mode'
+  } else {
+    html.classList.add('dark')
+    e.target.innerHTML = 'Light mode'
+  }
+})
