d189a58efb7057da6379501b90db64cb2d7d43e8
Carlos <carlosmontesinos1990@gmail.com>
2022-03-24 13:11:36 +0000

    comments added

--- Project 25 - Sticky Navbar/script.js
+++ Project 25 - Sticky Navbar/script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 150) {
+  if (window.scrollY > nav.offsetHeight + 150) { // To find out this measures, you can console log window.scrollY and check the values of each scrolling, the nav.offsetHeight will always be the same. So here we decide that the classList will change when the scroll value is higher than the nav height by 150.
     nav.classList.add('active')
   } else {
     nav.classList.remove('active')
